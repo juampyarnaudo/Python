ae505cc29be53293436c71d9692982963756b372
juampyarnaudo <iarnaudojuanpablo@gmail.com>
2023-01-26 10:45:07 -0300

    agregamos 07_funciones

---
+++ 07_func.py
@@ -0,0 +1,19 @@
+print('Hola')
+
+#escribimos la función
+def my_print(text):
+    #print('This is my print')
+    #print('This is my print 2')
+    print(text * 2)
+
+my_print('Este es mi texto')
+my_print('Hola')
+
+a = 10
+b = 90
+c= a+b
+def suma (a,b):
+    my_print(a+b)
+
+suma (1,5) #6
+suma (10,6) #16
